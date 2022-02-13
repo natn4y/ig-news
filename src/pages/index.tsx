a6f932ea98d24f8a975d6759b455a941d1f02358
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-13 20:37:38 -0300

    ⭐ SSR para SSG na conexão com a API do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -39,7 +39,7 @@ export default function Home({ subscription }: HomeProps) {
     </>
   );
 }
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const product_subscription = await stripe.prices.retrieve(
     "price_1KSm0vHowNYbIhfmOCcQUKQ6",
     {
@@ -60,5 +60,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       subscription,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
