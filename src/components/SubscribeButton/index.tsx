c19bcf2fb528d7e542864eab47af4efabc5ca090
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-14 17:21:07 -0300

    ⭐ Redirecionar user para tela de login caso o ele não esteja logado

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,26 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
 export function SubscribeButton() {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
