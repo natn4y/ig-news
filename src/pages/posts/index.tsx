3d3a648ccf11c26ed971969eb9cae3bb9ad5777b
Natanael Araújo <natanael@hotmail.co.jp>
2022-02-17 16:59:04 -0300

    ⭐ Exibição de posts através da API do prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,22 @@
 import { GetStaticProps } from "next";
+import * as RichText from "@prismicio/richtext";
+
 import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,21 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>01/05/1987</time>
-            <strong>title</strong>
-            <p>In the asdas kokd nnr mcikgmvir kdifmi mids</p>
-          </a>
-          <a href="">
-            <time>01/05/1987</time>
-            <strong>title</strong>
-            <p>In the asdas kokd nnr mcikgmvir kdifmi mids</p>
-          </a>
-          <a href="">
-            <time>01/05/1987</time>
-            <strong>title</strong>
-            <p>In the asdas kokd nnr mcikgmvir kdifmi mids</p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -40,10 +45,28 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   });
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
     revalidate: 60 * 60 * 24,
   };
 };
