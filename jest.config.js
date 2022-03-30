7f36fc1079357dcad91148b499a7cc87a21609f9
Natanael Araújo <natanael@hotmail.co.jp>
2022-03-30 10:40:16 -0300

    ☕ Update: Atualização das configurações do Jest

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  }
 };
